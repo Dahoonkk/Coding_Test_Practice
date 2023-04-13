16886e3702070b59f8b754055436e97d46920f0f
GuDahoon <iamkdh1@naver.com>
2023-04-13 16:31:22 +0900

    test commit4

--- Java_CodingTestPractice/src/practice/MultiplesOf3and5.java
+++ Java_CodingTestPractice/src/practice/MultiplesOf3and5.java
@@ -8,11 +8,16 @@ package practice;
 
 public class MultiplesOf3and5 {
 	public static void main(String[] args) {
+		int result = getTotal(3, 5, 1000);
+		System.out.println(result);
+	}
+
+	public static int getTotal(int a, int b, int maxNum) {
 		int total = 0;
-		for (int i=1; i<1000; i++) {
-			if(i%3==0 || i%5==0)
-				total += i;
+		for(int i=1; i<maxNum; i++) {
+			if(i%a==0 || i%b==0) total += i;
 		}
-		System.out.println(total);
+
+		return total;
 	}
 }
