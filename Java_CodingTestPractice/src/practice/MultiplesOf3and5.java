f582baf8d0bc51722faa26f7918ecf65a932e8f2
GuDahoon <iamkdh1@naver.com>
2023-04-13 16:27:52 +0900

    test commit2

--- Java_CodingTestPractice/src/practice/MultiplesOf3and5.java
+++ Java_CodingTestPractice/src/practice/MultiplesOf3and5.java
@@ -1,10 +1,16 @@
 package practice;
 
+/*
+ *   << 문제 >>
+ *   10미만의 자연수에서 3과 5의 배수를 구하면 3,5,6,9이다. 이들의 총합은 23이다.
+ *   1000미만의 자연수에서 3,5의 배수의 총합을 구하라.
+ */
+
 public class MultiplesOf3and5 {
 	public static void main(String[] args) {
 		int total = 0;
 		for (int i=1; i<=1000; i++) {
-			if(i%3==0 || i%5==0) 
+			if(i%3==0 || i%5==0)
 				total += i;
 		}
 		System.out.println(total);
